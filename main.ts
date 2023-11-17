33a738717d10f262452920477c71d75815bbcf1f
Muhammed58Sivas <151169604+Muhammed58Sivas@users.noreply.github.com>
2023-11-17 14:05:23 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,68 @@
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    if (Timeout == 0) {
+        if (KameraFolgt == 0) {
+            scene.cameraFollowSprite(mySprite2)
+            mySprite.sayText("du bist", 5000, false)
+            KameraFolgt = 1
+        } else {
+            scene.cameraFollowSprite(mySprite)
+            mySprite2.sayText("du bist", 5000, false)
+            KameraFolgt = 0
+        }
+        Timeout = 50
+    }
+})
+let KameraFolgt = 0
+let Timeout = 0
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . b 5 5 b . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . b b b b b 5 5 5 5 5 5 5 b . . 
+    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+    . . b d 5 5 b 1 f f 5 4 4 c . . 
+    b b d b 5 5 5 d f b 4 4 4 4 b . 
+    b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Enemy)
+Timeout = 0
+KameraFolgt = 0
+controller.moveSprite(mySprite)
+controller.player2.moveSprite(mySprite2)
+tiles.setCurrentTilemap(tilemap`Level1`)
+tiles.placeOnRandomTile(mySprite, sprites.castle.tileDarkGrass3)
+tiles.placeOnRandomTile(mySprite2, sprites.castle.tileGrass1)
+scene.cameraFollowSprite(mySprite)
+forever(function () {
+    if (Timeout != 0) {
+        Timeout += -1
+        pause(100)
+    }
+})
